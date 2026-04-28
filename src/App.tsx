f5f801eba5f3205494fd40e681c5fe431a5bb232
Janapareddi Vedha Varshini <jvvarshini2014245@gmail.com>
2026-04-28 13:52:20 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import Index from "./pages/Index.tsx";
 import NotFound from "./pages/NotFound.tsx";
+import { Blog } from "./pages/Blog";
 
 const queryClient = new QueryClient();
 
